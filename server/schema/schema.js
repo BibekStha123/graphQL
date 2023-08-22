5b18622eba3a71940878a83fec4860ecf097f592
bibekstha123 <bibekstha084@gmail.com>
2023-08-22 22:39:21 +0930

    project started

---
+++ server/schema/schema.js
@@ -0,0 +1,36 @@
+const { projects, clients } = require("../data.js");
+
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLSchema,
+} = require("graphql");
+
+// client type
+const ClientType = new GraphQLObjectType({
+  name: "Client",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    client: {
+      type: ClientType,
+      args: { id: { GraphQLID } },
+      resolve(parentValue, args) {
+        return clients.find((client) => client.id === args.id);
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
